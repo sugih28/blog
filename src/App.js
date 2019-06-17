ec63030f046a968075da1a9b63303c1a22c0f533
Sugih Ilmi Kalih Putra <sugih.ikp@gmail.com>
2019-06-17 15:58:30 +0700

    hide navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar'
-import {HashRouter, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <HashRouter>
       <div className="App">
-        <Navbar />
         <h2>Hai</h2>
       </div>
-    </HashRouter>
   );
 }
 
